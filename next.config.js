21efedf5970d3293935f5b5e47a72b76cd17bb11
cyrcop <cyrcop@gmail.com>
2025-10-20 21:44:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Désactive l'optimisation des images côté serveur (corrige l’erreur 504)
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
+module.exports = nextConfig;
